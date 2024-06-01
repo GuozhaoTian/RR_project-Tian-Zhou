6d501b830415bf1d8e022edb15a4d25b862df2bb
G. Tian <csjdtq@gmail.com>
2024-06-01 19:36:07 +0200

    Fixed the project environment, optimized the code and output images.

--- RR project/RR project.R
+++ RR project/RR project.R
@@ -6,11 +6,13 @@ library(urca)
 
 data <- read.csv("NFLX.csv")
 data$Date <- as.Date(data$Date, format = "%Y-%m-%d")
-data %>% glimpse()
+str(data)
+
 data <- xts(data[, -1],order.by = data$Date,frequency = 12)
 Adj.Close <- data$Adj.Close
+
 # Plot original Adjusted Close prices
-plot(Adj.Close, main = "NFLX Adjusted Close Prices", ylab = "Price", xlab = "Date")
+plot(Adj.Close, main = "NFLX Adjusted Close Prices", ylab = "Price", xlab = "Date", col = "sky blue")
 
 # Calculate weekly moving average (K=7)
 weekly_ma <- rollmean(Adj.Close, k = 7, fill = NA)
@@ -22,9 +24,8 @@ monthly_ma <- rollmean(Adj.Close, k = 30, fill = NA)
 yearly_ma <- rollmean(Adj.Close, k = 365, fill = NA)
 
 # Plot all moving averages
-plot(Adj.Close, main = "NFLX Adjusted Close Prices with Moving Averages", ylab = "Price", xlab = "Date")
-lines(weekly_ma, col = "blue", lwd = 2)
-lines(monthly_ma, col = "red", lwd = 2)
-lines(yearly_ma, col = "green", lwd = 2)
+plot(Adj.Close, main = "NFLX Adjusted Close Prices with Moving Averages", ylab = "Price", xlab = "Date", col = "red")
+lines(weekly_ma, col = "green", lwd = 2)
+lines(monthly_ma, col = "sky blue", lwd = 2)
+lines(yearly_ma, col = "purple", lwd = 2)
 legend("topright", legend = c("Weekly MA", "Monthly MA", "Yearly MA"), col = c("blue", "red", "green"), lwd = 2)
-
