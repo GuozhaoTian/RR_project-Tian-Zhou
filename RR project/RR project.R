be1b21512a68316b90e54c4d8313fc3967b8b2f8
yuezhou <y.zhou9@student.uw.edu.pl>
2024-06-02 13:28:48 +0200

    Add annotations and make predictions

--- RR project/RR project.R
+++ RR project/RR project.R
@@ -2,7 +2,7 @@ library(xts)
 library(lmtest)
 library(tidyverse)
 library(urca)
-
+library(forecast)
 # Read and prepare data
 data <- read.csv("NFLX.csv")
 data$Date <- as.Date(data$Date, format = "%Y-%m-%d")
@@ -46,87 +46,223 @@ plot(decomp$random, main = "Random Component", ylab = "Remainder")
 par(mfrow = c(1, 1), mar = c(1, 1, 1, 1)) 
 
 
-
+##The Augmented Dickey-Fuller (ADF) test for unit roots in a time series..
 adf_test <- ur.df(Adj.Close$Adj.Close, type = "none", lags = 0)
 summary(adf_test)
-
+# Set up the plotting area to have 2 rows and 1 column
 par(mfrow = c(2, 1)) 
 # Set smaller margins
 par(mar = c(5, 5, 2, 2))  # c(bottom, left, top, right)
 
-# Plot your ACF with adjusted margins
+# Plot your ACF and PACF with adjusted margins
 acf(Adj.Close$Adj.Close, lag.max = 60, ylim = c(0.0, 1), lwd = 5, col = "grey", na.action = na.pass)
-
 pacf(Adj.Close$Adj.Close,lag.max = 36,lwd = 5, col = "black",na.action = na.pass)
+# Reset the plotting area to the default
 par(mfrow = c(1, 1)) 
 
-
+# Difference the data and remove NAs
 dadj <- na.exclude(diff.xts(Adj.Close$Adj.Close))
+
+# Perform the ADF test on the differenced data
 adf_test_dadj <- ur.df(dadj, type = "none", lags = 0)
 summary(adf_test_dadj)
-
+# Set up the plotting area to have 2 rows and 1 column
 par(mfrow = c(2, 1)) 
 par(mar = c(5, 5, 2, 2))  # c(bottom, left, top, right)
-acf(dadj,lag.max = 60, ylim = c(0.0, 0.1),lwd = 5,col = "dark green",na.action = na.pass)  
+# Plot the ACF
+acf(dadj,lag.max = 60, ylim = c(0.0, 0.1),lwd = 5,col = "dark green",na.action = na.pass) 
+# Plot the PACF
 pacf(dadj,lag.max = 36,lwd = 5, col = "dark green",na.action = na.pass)
+# Reset the plotting area to the default
 par(mfrow = c(1, 1)) 
 
 
-library(forecast)
+# Automatically fit ARIMA model
 auto.arima(Adj.Close)
+# Subset data up to a specific date
 cdata.shortx1 <- Adj.Close$Adj.Close["/20171007", ]
+# Fit ARIMA(2, 1, 2) model
 arima212 <- Arima(cdata.shortx1$Adj.Close,order = c(2, 1, 2))
 arima212
+# Coefficient test and summary
 coeftest(arima212)
 summary(arima212)
+# Plot residuals
 plot(resid(arima212))
 tibble(date = index(cdata.shortx1$Adj.Close),resid = arima212 %>% resid() %>% as.numeric()
 ) %>%
   ggplot(aes(date, resid)) +
   geom_line(col = "royalblue3") +theme_bw()
 
-
+# Set up the plotting area to have 2 rows and 1 column
 par(mfrow = c(2, 1)) 
 par(mar = c(5, 5, 2, 2))  # c(bottom, left, top, right)
+
+# ACF and PACF of residuals
 acf(resid(arima212),lag.max = 60,ylim = c(-0.1, 0.1),lwd = 5, col = "dark green",na.action = na.pass)
 pacf(resid(arima212),lag.max = 36,lwd = 5, col = "dark green",na.action = na.pass)
+# Reset the plotting area to the default
 par(mfrow = c(1, 1))
+# Ljung-Box test
+# The Box.test checks for the presence of autocorrelation in residuals. 
 Box.test(resid(arima212), type = "Ljung-Box", lag = 4)
-
+# Fit ARIMA(1, 2, 33) model
 arima1133 <- Arima(cdata.shortx1$Adj.Close,order = c(1, 1,33))
 arima1133
+# Coefficient test and summary
 coeftest(arima1133)
 summary(arima1133)
+# Plot residuals
 plot(resid(arima1133))
 tibble(date = index(cdata.shortx1$Adj.Close),resid = arima1133 %>% resid() %>% as.numeric()
 ) %>%
   ggplot(aes(date, resid)) +
   geom_line(col = "royalblue3") +theme_bw()
 
-
+# Set up the plotting area to have 2 rows and 1 column
 par(mfrow = c(2, 1)) 
 par(mar = c(5, 5, 2, 2))  # c(bottom, left, top, right)
+# ACF and PACF of residuals
 acf(resid(arima1133),lag.max = 60,ylim = c(-0.1, 0.1),lwd = 5, col = "dark green",na.action = na.pass)
 pacf(resid(arima1133),lag.max = 36,lwd = 5, col = "dark green",na.action = na.pass)
+# Reset the plotting area to the default
 par(mfrow = c(1, 1))
+# Ljung-Box test
+# The Box.test checks for the presence of autocorrelation in residuals. 
 Box.test(resid(arima1133), type = "Ljung-Box", lag = 4)
 
 
-
+# Fit ARIMA(1, 2, 33) model
 arima1233 <- Arima(cdata.shortx1$Adj.Close,order = c(1, 2,33))
 arima1233
+# Coefficient test and summary
 coeftest(arima1233)
 summary(arima1233)
+# Plot residuals
 plot(resid(arima1233))
 tibble(date = index(cdata.shortx1$Adj.Close),resid = arima1233 %>% resid() %>% as.numeric()
 ) %>%
   ggplot(aes(date, resid)) +
   geom_line(col = "royalblue3") +theme_bw()
 
-
+# Set up the plotting area to have 2 rows and 1 column
 par(mfrow = c(2, 1)) 
 par(mar = c(5, 5, 2, 2))  # c(bottom, left, top, right)
+# ACF and PACF of residuals
 acf(resid(arima1233),lag.max = 60,ylim = c(-0.1, 0.1),lwd = 5, col = "dark green",na.action = na.pass)
 pacf(resid(arima1233),lag.max = 36,lwd = 5, col = "dark green",na.action = na.pass)
+# Reset the plotting area to the default
 par(mfrow = c(1, 1))
+# Ljung-Box test
+# The Box.test checks for the presence of autocorrelation in residuals. 
 Box.test(resid(arima1233), type = "Ljung-Box", lag = 4)
+
+# Generate forecasts,627 future values are forecasted.
+forecasts <- forecast(arima212, h = 627) 
+
+# Extract forecasted mean
+forecasts
+forecasts$mean
+# Check class and convert to numeric
+class(forecasts$mean)
+as.numeric(forecasts$mean)
+# Extract forecast confidence intervals
+forecasts$lower
+forecasts$upper
+# Create data frame for forecast data
+forecasts_data <- data.frame(f_mean  = as.numeric(forecasts$mean),
+                             f_lower = as.numeric(forecasts$lower[, 2]),
+                             f_upper = as.numeric(forecasts$upper[, 2]))
+# Display the forecast data
+forecasts_data 
+
+
+# Subset data up to a specific date
+cdata.shortx1 <- Adj.Close$Adj.Close["/20171007", ]
+cdata.shortx1
+# Check the tail of the subset data
+tail(cdata.shortx1)
+# Subset data from a specific date
+cdata.shortx2 <-Adj.Close$Adj.Close["20171007/", ]
+# Convert forecast data to xts object
+forecasts_xts <- xts(forecasts_data,order.by =index(cdata.shortx2))
+# Merge original data with forecast data
+cdata3 <- merge(Adj.Close,forecasts_xts)
+# Display the merged data
+head(cdata3)
+tail(cdata3, n =627)
+# Plot the merged data
+plot(cdata3, main = "Original Data and Forecasts", col = c("black", "blue", "red", "green"))
+
+
+# Generate forecasts,627 future values are forecasted.
+forecasts <- forecast(arima1133, h = 627) 
+forecasts
+# Extract forecasted mean
+forecasts$mean
+# Check class and convert to numeric
+class(forecasts$mean)
+as.numeric(forecasts$mean)
+# Extract forecast confidence intervals
+forecasts$lower
+forecasts$upper
+# Create data frame for forecast data
+forecasts_data <- data.frame(f_mean  = as.numeric(forecasts$mean),
+                             f_lower = as.numeric(forecasts$lower[, 2]),
+                             f_upper = as.numeric(forecasts$upper[, 2]))
+
+# Display the forecast data
+forecasts_data 
+
+
+# Subset data up to a specific date
+cdata.shortx1 <- Adj.Close$Adj.Close["/20171007", ]
+cdata.shortx1
+# Check the tail of the subset data
+tail(cdata.shortx1)
+# Subset data from a specific date
+cdata.shortx2 <-Adj.Close$Adj.Close["20171007/", ]
+# Convert forecast data to xts object
+forecasts_xts <- xts(forecasts_data,order.by =index(cdata.shortx2))
+# Merge original data with forecast data
+cdata3 <- merge(Adj.Close,forecasts_xts)
+# Display the merged data
+head(cdata3)
+tail(cdata3, n =627)
+# Plot the merged data
+plot(cdata3, main = "Original Data and Forecasts", col = c("black", "blue", "red", "green"))
+
+forecasts <- forecast(arima1233, h = 627) 
+forecasts
+# Extract forecasted mean
+forecasts$mean
+# Check class and convert to numeric
+class(forecasts$mean)
+as.numeric(forecasts$mean)
+# Extract forecast confidence intervals
+forecasts$lower
+forecasts$upper
+# Create data frame for forecast data
+forecasts_data <- data.frame(f_mean  = as.numeric(forecasts$mean),
+                             f_lower = as.numeric(forecasts$lower[, 2]),
+                             f_upper = as.numeric(forecasts$upper[, 2]))
+# Display the forecast data
+forecasts_data 
+
+
+# Subset data up to a specific date
+cdata.shortx1 <- Adj.Close$Adj.Close["/20171007", ]
+cdata.shortx1
+# Check the tail of the subset data
+tail(cdata.shortx1)
+# Subset data from a specific date
+cdata.shortx2 <-Adj.Close$Adj.Close["20171007/", ]
+# Convert forecast data to xts object
+forecasts_xts <- xts(forecasts_data,order.by =index(cdata.shortx2))
+# Merge original data with forecast data
+cdata3 <- merge(Adj.Close,forecasts_xts)
+# Display the merged data
+head(cdata3)
+tail(cdata3, n =627)
+# Plot the merged data
+plot(cdata3, main = "Original Data and Forecasts", col = c("black", "blue", "red", "green"))
